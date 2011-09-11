e594defb9d3795ff56ece0456b9a26612f7e578c
Felix Schäfer <felix@fachschaften.org>
2011-09-11 20:08:40 +0200

    Make {% img %} more flexible.

--- plugins/image_tag.rb
+++ plugins/image_tag.rb
@@ -1,36 +1,44 @@
 # Title: Simple Image tag for Jekyll
-# Author: Brandon Mathis http://brandonmathis.com
-# Description: Easily output images with optional class names and title/alt attributes
+# Authors: Brandon Mathis http://brandonmathis.com
+#          Felix Schäfer
+# Description: Easily output images with optional class names, width, height, title and alt attributes
 #
-# Syntax {% image [class name(s)] url [title text] %}
+# Syntax {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | "title text" ["alt text"]] %}
 #
-# Example:
-# {% ima left half http://site.com/images/ninja.png Ninja Attack! %}
+# Examples:
+# {% img /images/ninja.png Ninja Attack! %}
+# {% img left half http://site.com/images/ninja.png Ninja Attack! %}
+# {% img left half http://site.com/images/ninja.png 150 150 "Ninja Attack!" "Ninja in attack posture" %}
 #
 # Output:
-# <image class='left' src="http://site.com/images/ninja.png" title="Ninja Attack!" alt="Ninja Attack!">
+# <img src='/images/ninja.png'>
+# <img class='left half' src='http://site.com/images/ninja.png' title='Ninja Attack!' alt='Ninja Attack!'>
+# <img class='left half' src='http://site.com/images/ninja.png' width='150' height='150' title='Ninja Attack!' alt='Ninja in attack posture'>
 #
 
 module Jekyll
-
   class ImageTag < Liquid::Tag
-    @img = nil
-    @title = nil
-    @class = ''
-    @width = ''
-    @height = ''
+    TAGS = [:class, :src, :width, :height, :title, :alt]
+
+    CLASS = /(?:(?<class>[^'"\s][^'"]*)\s+)/
+    SRC = /(?:(?<src>(https?:\/\/|\/)([^'"\s]+)))/i
+    WIDTH = /(?:\s+(?<width>\d+))/
+    HEIGHT = /(?:\s+(?<height>\d+))/
+    DIMENSIONS = /#{WIDTH}(?:#{HEIGHT})?/  # height is optional
+    UNQUOTED_TITLE = /(?:\s+(?<title>[^'"\s][^'"]*?))/
+    QUOTED_TITLE = /(?:\s+(?<title_quote>['"])(?<quoted_title>[^'"]*)\g<title_quote>)/
+    QUOTED_ALT = /(?:\s+(?<alt_quote>['"])(?<alt>[^'"]*)\g<alt_quote>)/
+    TITLE_AND_ALT = /(?:#{UNQUOTED_TITLE}|#{QUOTED_TITLE}(?:#{QUOTED_ALT})?)/  # alt is optional
+    IMG = /\A#{CLASS}?#{SRC}#{DIMENSIONS}?#{TITLE_AND_ALT}?\s*\Z/  # class, dimensions title and alt are optional
 
     def initialize(tag_name, markup, tokens)
-      if markup =~ /(\S.*\s+)?(https?:\/\/|\/)(\S+)(\s+\d+\s+\d+)?(\s+.+)?/i
-        @class = $1 || ''
-        @img = $2 + $3
-        if $5
-          @title = $5.strip
-        end
-        if $4 =~ /\s*(\d+)\s+(\d+)/
-          @width = $1
-          @height = $2
-        end
+      puts "foo " + markup.inspect
+      if markup =~ IMG
+        @img = TAGS.reduce({}) {|img,tag| img[tag] = $~[tag]; img}
+        @img[:title] ||= $~[:quoted_title]
+        # Make sure alt is set if we have something to put in there
+        @img[:alt] ||= @img[:title]
+        puts @img.inspect
       end
       super
     end
@@ -38,9 +46,9 @@ module Jekyll
     def render(context)
       output = super
       if @img
-        "<img class='#{@class}' src='#{@img}' width='#{@width}' height='#{@height}' alt='#{@title}' title='#{@title}'>"
+        "<img #{@img.reduce([]) {|a,p| a << "#{p[0].to_s}='#{p[1]}'" if p[1]; a}.join(" ")}>"
       else
-        "Error processing input, expected syntax: {% img [class name(s)] /url/to/image [width height] [title text] %}"
+        "Error processing input, expected syntax: {% img [class name(s)] /url/to/image [width [height]] [title text | \"title text\" [\"alt text\"]] %}"
       end
     end
   end
