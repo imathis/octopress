a2333825a6bd371c37585a84ff0fb331316077fe
Brandon Mathis <brandon@imathis.com>
2011-12-10 12:57:15 -0600

    updated to latest

--- source/javascripts/octopress.js
+++ source/javascripts/octopress.js
@@ -3,7 +3,10 @@ function getNav() {
   mobileNav.children('select').append('<option value="">Navigate&hellip;</option>');
   $('ul[role=main-navigation]').addClass('main-navigation');
   $('ul.main-navigation a').each(function(link) {
-    mobileNav.children('select').append('<option value="'+link.href+'">&bull; '+link.text+'</option>');
+    mobileNav.children('select').append('<option value="'+link.href+'">&raquo; '+link.text+'</option>');
+  });
+  $('ul.subscription a').each(function(link) {
+    mobileNav.children('select').append('<option value="'+link.href+'">&raquo; '+link.text+'</option>');
   });
   mobileNav.children('select').bind('change', function(event) {
     if (event.target.value) { window.location.href = event.target.value; }
