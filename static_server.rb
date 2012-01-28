fa96d270b901a1f72ec6db59aa2f0f2dc3eb1267
James Ottaway <james@ottaway.mp>
2012-01-28 22:28:01 +1000

    Split StaticServer class out of config.ru to make it a little more flexible

---
+++ static_server.rb
@@ -0,0 +1,21 @@
+require 'bundler/setup'
+require 'sinatra/base'
+
+# The project root directory
+$root = ::File.dirname(__FILE__)
+
+class SinatraStaticServer < Sinatra::Base  
+  get(/.+/) do
+    send_sinatra_file(request.path) {404}
+  end
+
+  not_found do
+    send_sinatra_file('404.html') {"Sorry, I cannot find #{request.path}"}
+  end
+
+  def send_sinatra_file(path, &missing_file_block)
+    file_path = File.join(File.dirname(__FILE__), 'public',  path)
+    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i  
+    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
+  end
+end
