b69e876828510c583fa74f6b52641ae8f0d733b6
Maxwell Swadling <maxwellswadling@gmail.com>
2012-04-18 14:49:58 +1000

    Fixed paths in GitHub api lists from using api urls instead of HTML urls.
    Signed-off-by: Brandon Mathis <brandon@imathis.com>

--- .themes/classic/source/javascripts/github.js
+++ .themes/classic/source/javascripts/github.js
@@ -3,7 +3,7 @@ var github = (function(){
     var i = 0, fragment = '', t = $(target)[0];
 
     for(i = 0; i < repos.length; i++) {
-      fragment += '<li><a href="'+repos[i].url+'">'+repos[i].name+'</a><p>'+repos[i].description+'</p></li>';
+      fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a><p>'+repos[i].description+'</p></li>';
     }
     t.innerHTML = fragment;
   }
