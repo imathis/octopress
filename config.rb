44118878d781a0aed3613cfae8a1d803bd5c95d8
Brandon Mathis <brandon@imathis.com>
2012-06-05 12:39:23 -0500

    Changed do_not_track setting to respectfully_social. Added a better description to the config file. Also, now social icons are sprited for bandwidth friendliness

--- config.rb
+++ config.rb
@@ -4,6 +4,7 @@ project_type = :stand_alone
 # Publishing paths
 http_path = "/"
 http_images_path = "/images"
+http_generated_images_path = "/images"
 http_fonts_path = "/fonts"
 css_dir = "public/stylesheets"
 
@@ -11,6 +12,7 @@ css_dir = "public/stylesheets"
 sass_dir = "sass"
 images_dir = "source/images"
 fonts_dir = "source/fonts"
+generated_images_dir = "source/images"
 
 line_comments = false
 output_style = :compressed
