1c8a4de58e377807b0765cacbb0d75725764e7bc
Brandon Mathis <brandon@imathis.com>
2012-06-05 00:35:13 -0500

    Fixed issue where gists wouldn't get line numbers added. Fixed issue where backtick code blocks were getting empty figcaptions

--- .themes/classic/source/javascripts/octopress.js
+++ .themes/classic/source/javascripts/octopress.js
@@ -40,17 +40,17 @@ function addSidebarToggler() {
 
 function addCodeLineNumbers() {
   if (navigator.appName === 'Microsoft Internet Explorer') { return; }
-  $('div.gist-highlight').each(function(code) {
+  $('div.gist-highlight').each(function(index) {
     var tableStart = '<table><tbody><tr><td class="gutter">',
         lineNumbers = '<pre class="line-numbers">',
         tableMiddle = '</pre></td><td class="code">',
         tableEnd = '</td></tr></tbody></table>',
-        count = $('.line', code).length;
+        count = $('.line', this).length;
     for (var i=1;i<=count; i++) {
       lineNumbers += '<span class="line-number">'+i+'</span>\n';
     }
-    var table = tableStart + lineNumbers + tableMiddle + '<pre>'+$('pre', code).html()+'</pre>' + tableEnd;
-    $(code).html(table);
+    var table = tableStart + lineNumbers + tableMiddle + '<pre>'+$('pre', this).html()+'</pre>' + tableEnd;
+    $(this).html(table);
   });
 }
 
