d102dcda688191af6845322c91e7a66be688b0ee
Leonardo Saraiva <vyper@maneh.org>
2012-06-19 01:05:15 -0300

    Added stringex and changed to use #to_url for create directory. To don't receive error in url to accented characters.

--- plugins/category_generator.rb
+++ plugins/category_generator.rb
@@ -19,6 +19,8 @@
 # - category_title_prefix: The string used before the category name in the page title (default is
 #                          'Category: ').
 
+require 'stringex'
+
 module Jekyll
 
   # The CategoryIndex class creates a single category page for the specified category.
@@ -106,7 +108,7 @@ module Jekyll
       if self.layouts.key? 'category_index'
         dir = self.config['category_dir'] || 'categories'
         self.categories.keys.each do |category|
-          self.write_category_index(File.join(dir, category.gsub(/_|\P{Word}/, '-').gsub(/-{2,}/, '-').downcase), category)
+          self.write_category_index(File.join(dir, category.to_url), category)
         end
 
       # Throw an exception if the layout couldn't be found.
@@ -143,7 +145,7 @@ module Jekyll
     def category_links(categories)
       dir = @context.registers[:site].config['category_dir']
       categories = categories.sort!.map do |item|
-        "<a class='category' href='/#{dir}/#{item.gsub(/_|\P{Word}/, '-').gsub(/-{2,}/, '-').downcase}/'>#{item}</a>"
+        "<a class='category' href='/#{dir}/#{item.to_url}/'>#{item}</a>"
       end
 
       case categories.length
