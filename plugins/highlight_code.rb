fe42025b4fae11c7d318718d363cde2c1ccea79e
Jay Adkisson <jjmadkisson@gmail.com>
2012-10-30 18:54:24 +0100

    integrate rouge line numbers
    and update the styles to reflect the new markup

--- plugins/highlight_code.rb
+++ plugins/highlight_code.rb
@@ -5,7 +5,7 @@ require 'digest/md5'
 module HighlightCode
   def highlight(str, lang)
     lexer = Rouge::Lexer.find_fancy(lang) || Rouge::Lexers::Text
-    formatter = Rouge::Formatters::HTML.new(:css_class => 'pre-code')
+    formatter = Rouge::Formatters::HTML.new(:line_numbers => true)
     pre = formatter.format(lexer.lex(str))
     %<<code class=#{lang.inspect}>#{pre}</code>>
   end
