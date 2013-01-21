fa2aa4913ded303e4c3c83fe872fe1555f778324
Parker Moore <parkrmoore@gmail.com>
2013-01-20 23:43:22 -0500

    Added Octopress.env

--- lib/octopress.rb
+++ lib/octopress.rb
@@ -1,7 +1,12 @@
 $:.unshift File.dirname(__FILE__) # For use/testing when no gem is installed
 
-module Octopress
-end
-
 require "octopress/core_ext"
 require "octopress/configuration"
+
+module Octopress
+
+  # Static: Fetches the Octopress environment
+  def self.env
+    ENV["OCTOPRESS_ENV"] || Configuration.read_config("defaults/jekyll.yml")[:env]
+  end
+end
