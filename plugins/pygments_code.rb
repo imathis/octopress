35e23f59ca00a04c1784180b765c3e6cc23f9a40
Shane Liesegang <shane@techie.net>
2013-01-30 21:51:26 -0500

    Update plugins/pygments_code.rb
    Giving a more helpful error message instead of silently failing.

--- plugins/pygments_code.rb
+++ plugins/pygments_code.rb
@@ -23,8 +23,8 @@ module HighlightCode
       else
         begin
           highlighted_code = Pygments.highlight(code, :lexer => lang, :formatter => 'html', :options => {:encoding => 'utf-8'})
-        rescue Exception => e
-          highlighted_code = Pygments.highlight(code, :lexer => 'text', :formatter => 'html', :options => {:encoding => 'utf-8'})
+        rescue MentosError
+          raise "Pygments can't parse unknown language: #{lang}."
         end
         File.open(path, 'w') {|f| f.print(highlighted_code) }
       end
