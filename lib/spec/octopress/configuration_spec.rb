acd2ba718ee7e38f611a1ed832ac1afbcec1e136
Jon Frisby <jfrisby@mrjoy.com>
2013-02-23 23:30:22 -0800

    Adding first pass at tests around Octopress.env...

--- lib/spec/octopress/configuration_spec.rb
+++ lib/spec/octopress/configuration_spec.rb
@@ -1,13 +1,54 @@
 require 'minitest/autorun'
 require_relative '../../octopress'
 
+describe Octopress do
+  describe ".configurator" do
+    before do
+      Octopress.clear_config!
+      @old_env = ENV['OCTOPRESS_ENV']
+      ENV['OCTOPRESS_ENV'] = nil
+    end
+
+    after do
+      ENV['OCTOPRESS_ENV'] = @old_env
+    end
+
+    it "should accept a path pointing to a config directory" do
+      Octopress.configurator(File.join(File.dirname(__FILE__), '../', 'fixtures', 'env'))
+
+      Octopress.env.must_equal 'config_specified_environment'
+    end
+  end
+
+  describe ".configuration" do
+    before do
+      Octopress.clear_config!
+      @old_env = ENV['OCTOPRESS_ENV']
+      ENV['OCTOPRESS_ENV'] = nil
+      Octopress.configurator(File.join(File.dirname(__FILE__), '../', 'fixtures', 'env'))
+    end
+
+    after do
+      ENV['OCTOPRESS_ENV'] = @old_env
+    end
+
+    subject do
+      Octopress.configuration
+    end
+
+    it "should provide access to the specified configuration" do
+      subject[:env].must_equal 'config_specified_environment'
+    end
+  end
+end
+
 describe Octopress::Configuration do
   describe '#read_configuration' do
     describe "when no override" do
       before do
         @octo_config = Octopress::Configuration.new(File.join(File.dirname(__FILE__), '../', 'fixtures', 'no_override'))
       end
-      
+
       subject do
         @octo_config.read_configuration
       end
