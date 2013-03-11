75cc24c8d602d509e6b272db983489090595f001
Brandon Mathis <brandon@imathis.com>
2013-03-11 12:48:10 -0500

    fixed addSidebarToggler where index and section were flip-flopped

--- .themes/classic/source/javascripts/octopress.js
+++ .themes/classic/source/javascripts/octopress.js
@@ -28,7 +28,7 @@ function addSidebarToggler() {
   }
   var sections = $('aside.sidebar > section');
   if (sections.length > 1) {
-    sections.each(function(section, index){
+    sections.each(function(index, section){
       if ((sections.length >= 3) && index % 3 === 0) {
         $(section).addClass("first");
       }
