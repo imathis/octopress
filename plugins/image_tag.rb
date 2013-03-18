39fca91b699f8a881e638a2e3da63ed8d3114f43
Brandon Mathis <brandon@imathis.com>
2013-03-17 23:56:46 -0500

    fixed example comments in image_tag plugin

--- plugins/image_tag.rb
+++ plugins/image_tag.rb
@@ -8,12 +8,12 @@
 # Examples:
 # {% img /images/ninja.png Ninja Attack! %}
 # {% img left half http://site.com/images/ninja.png Ninja Attack! %}
-# {% img left half http://site.com/images/ninja.png 150 150 "Ninja in attack posture" "Ninja Attack!" %}
+# {% img left half http://site.com/images/ninja.png 150 150 alt:"Ninja in attack posture" title:"Ninja Attack!" %}
 #
 # Output:
 # <img src="/images/ninja.png">
-# <img class="left half" src="http://site.com/images/ninja.png" title="Ninja Attack!" alt="Ninja Attack!">
-# <img class="left half" src="http://site.com/images/ninja.png" width="150" height="150" title="Ninja Attack!" alt="Ninja in attack posture">
+# <img class="left half" src="http://site.com/images/ninja.png" alt="Ninja Attack!">
+# <img class="left half" src="http://site.com/images/ninja.png" width="150" height="150" alt="Ninja in attack posture" title="Ninja Attack!">
 #
 
 module Jekyll
