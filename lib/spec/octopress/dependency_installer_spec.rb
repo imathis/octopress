7e9213af7de1bfb647c88ba73dbdb6af64ee70d0
Parker Moore <parkrmoore@gmail.com>
2013-03-20 02:01:50 +0100

    RSpec for DepenedencyInstaller#git_url. Making progress.

--- lib/spec/octopress/dependency_installer_spec.rb
+++ lib/spec/octopress/dependency_installer_spec.rb
@@ -1,5 +1,12 @@
-require 'minitest/autorun'
-require_relative '../../octopress'
+require_relative '../spec_helper.rb'
 
 describe Octopress::DependencyInstaller do
+  describe "#git_url" do
+    let(:installer) { Octopress::DependencyInstaller.new }
+    describe "when just a repo name is specified" do
+      let(:plugin) { "adn-timeline" }
+      subject { Octopress::DependencyInstaller.new.git_url(plugin) }
+      it { should eq("git://github.com/octopress/#{plugin}.git") }
+    end
+  end
 end
