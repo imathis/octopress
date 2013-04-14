937bbe184964f5658921fa045c36d2159dc7325f
Jon Frisby <jfrisby@mrjoy.com>
2013-04-13 18:01:19 -0700

    Make InquirableString be comparable to symbols.
    (I documented it this way elsewhere, and I'm too lazy to find where that was, so...)

--- lib/octopress/inquirable_string.rb
+++ lib/octopress/inquirable_string.rb
@@ -1,6 +1,8 @@
 module Octopress
   # An extended version of String that supports Rails style `.foo?` calls to
-  # test for equality.
+  # test for equality, and allows for direct comparison against both string and
+  # symbol values.  (Never again worry about whether some method is returning a
+  # string or a symbol!)
   #
   # For example:
   #
@@ -9,6 +11,7 @@ module Octopress
   # tmp.something?      # Returns `true`.
   # tmp.something_else? # Returns `false`.
   # tmp == 'something'  # Returns `true`.
+  # tmp == :something   # Returns `true`.
   # ```
   class InquirableString < String
     # A handler that captures methods of the form `<some_string>?`, and does
@@ -21,6 +24,23 @@ module Octopress
         return self == val
       else
         super
+      end
+    end
+
+    # A little magic to ensure we can compare against strings, symbols, and
+    # other InquirableString objects and always get the results one would
+    # predict we'd get.
+    #
+    # other_val - The value to compare against this one.
+    #
+    # Returns true if they are logically equivalent, false otherwise.
+    def ==(other_val)
+      if(other_val.is_a?(Symbol) || other_val.is_a?(InquirableString))
+        return other_val.to_s == self
+      elsif(other_val.is_a?(String))
+        return other_val == self
+      else
+        return false
       end
     end
   end
