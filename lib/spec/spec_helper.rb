77ecc71debd5266dc297fffbfad3a31fe7c592b8
Jon Frisby <jfrisby@mrjoy.com>
2013-05-28 14:00:06 -0700

    Fix tyop -- path was being concat'd incorrectly.

--- lib/spec/spec_helper.rb
+++ lib/spec/spec_helper.rb
@@ -6,7 +6,7 @@ require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Octopress.lib_root.concat("spec/support/**/*.rb")].each {|f| require f}
+Dir[Octopress.lib_root.concat("/spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   # Include FactoryGirl helpers
