283d362710ff4072ac2f19f27cd84e29a70e5c77
Jim Cushing <jimothy@mac.com>
2013-06-07 18:41:26 -0400

    sets output style to expanded if in development mode

--- config.rb
+++ config.rb
@@ -15,4 +15,4 @@ images_dir = "source/images"
 fonts_dir = "source/fonts"
 
 line_comments = false
-output_style = :compressed
+output_style = (ENV['env'] == 'development') ? :expanded : :compressed
