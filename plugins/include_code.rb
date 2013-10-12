4fd754a76207c1c55f657891dc7049cf76f5f27e
Brandon Mathis <brandon@imathis.com>
2013-10-12 16:08:59 -0500

    Removed space to fix HAML compatibility on include_code.

--- plugins/include_code.rb
+++ plugins/include_code.rb
@@ -62,7 +62,7 @@ module Jekyll
         title = @title ? "#{@title} (#{file.basename})" : file.basename
         url = "/#{code_dir}/#{@file}"
         source = "<figure class='code'><figcaption><span>#{title}</span> <a href='#{url}'>download</a></figcaption>\n"
-        source += " #{highlight(code, @filetype)}</figure>"
+        source += "#{highlight(code, @filetype)}</figure>"
         safe_wrap(source)
       end
     end
