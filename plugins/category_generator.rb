628e0e4d9ab6d251991fb93b187f10eebff3d7a8
AR <aleiphoenix@gmail.com>
2013-10-24 22:59:03 +0800

    fix minor mis-spelling

--- plugins/category_generator.rb
+++ plugins/category_generator.rb
@@ -119,7 +119,7 @@ module Jekyll
 ===============================================
  Error for category_generator.rb plugin
 -----------------------------------------------
- No 'category_index.hmtl' in source/_layouts/
+ No 'category_index.html' in source/_layouts/
  Perhaps you haven't installed a theme yet.
 ===============================================
 
@@ -191,4 +191,3 @@ ERR
   end
 
 end
-
