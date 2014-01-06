imathis/octopress

commit 7dfba9a26e21b970f74aa663a86d407ae8fd5958

fixed video wrapping js

.themes/classic/source/javascripts/octopress.js
@@ -72,10 +72,10 @@ function addCodeLineNumbers() {
 function flashVideoFallback(){
   var flashplayerlocation = "/assets/jwplayer/player.swf",
       flashplayerskin = "/assets/jwplayer/glow/glow.xml";
-  $('video').each(function(video){
+  $('video').each(function(i, video){
     video = $(video);
     if (!Modernizr.video.h264 && swfobject.getFlashPlayerVersion() || window.location.hash.indexOf("flash-test") !== -1){
-      video.children('source[src$=mp4]').first().map(function(i, source){
+      video.children('source[src$=mp4]').first().map(i, function(source){
         var src = $(source).attr('src'),
             id = 'video_'+Math.round(1 + Math.random()*(100000)),
             width = video.attr('width'),
@@ -92,18 +92,12 @@ function flashVideoFallback(){
 }
 
 function wrapFlashVideos() {
-  $('object').each(function(object) {
-    object = $(object);
-    if ( $('param[name=movie]', object).length ) {
-      var wrapper = object.before('<div class="flash-video"><div>').previous();
-      $(wrapper).children().append(object);
+  $('object').each(function(i, object) {
+    if( $(object).find('param[name=movie]').length ){
+      $(object).wrap('<div class="flash-video">')
     }
   });
-  $('iframe[src*=vimeo],iframe[src*=youtube]').each(function(iframe) {
-    iframe = $(iframe);
-    var wrapper = iframe.before('<div class="flash-video"><div>').previous();
-    $(wrapper).children().append(iframe);
-  });
+  $('iframe[src*=vimeo],iframe[src*=youtube]').wrap('<div class="flash-video">')
 }
 
 function renderDeliciousLinks(items) {

commit e3ad9f6736d3fe3458627fd2a26d513ddfd65f43

Embedded Flash videos are responsive again
Fix for bug introduced in 7dfba9a26e21b970f74aa663a86d407ae8fd5958.

.themes/classic/source/javascripts/octopress.js
@@ -94,10 +94,10 @@ function flashVideoFallback(){
 function wrapFlashVideos() {
   $('object').each(function(i, object) {
     if( $(object).find('param[name=movie]').length ){
-      $(object).wrap('<div class="flash-video">')
+      $(object).wrap('<div class="flash-video"><div>')
     }
   });
-  $('iframe[src*=vimeo],iframe[src*=youtube]').wrap('<div class="flash-video">')
+  $('iframe[src*=vimeo],iframe[src*=youtube]').wrap('<div class="flash-video"><div>');
 }
 
 function renderDeliciousLinks(items) {