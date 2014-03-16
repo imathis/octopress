4edff85a97230eae4fcc1224a39b6aecc55a256d
Luke Dodge <luke@brewpuppy.net>
2014-03-16 15:12:20 -0500

    Add option to hide the Github Pages repository from the Github aside

--- .themes/classic/source/javascripts/github.js
+++ .themes/classic/source/javascripts/github.js
@@ -21,6 +21,7 @@ var github = (function(){
           if (!data || !data.data) { return; }
           for (var i = 0; i < data.data.length; i++) {
             if (options.skip_forks && data.data[i].fork) { continue; }
+            if (options.skip_github_pages && data.data[i].name == (options.user+'.github.io')) { continue; }
             repos.push(data.data[i]);
           }
           if (options.count) { repos.splice(options.count); }
