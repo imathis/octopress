1e64a6e143a6718f05a8d7152f689eea76209303
Christopher Webber <cwebber@getchef.com>
2014-05-20 06:44:45 -0700

    Make the bucket first...

--- bin/deploy_if_not_master.sh
+++ bin/deploy_if_not_master.sh
@@ -10,9 +10,9 @@ if [ "${CI_BRANCH}" != "master" ]; then
   # and underscores, remember... you did it to yourself
 
   pip install awscli
+  aws s3 mb s3://${CI_BRANCH}.cwebber.net
   aws s3 rm s3://${CI_BRANCH}.cwebber.net --recursive
   aws s3 sync public s3://${CI_BRANCH}.cwebber.net --acl public-read
   aws s3api put-bucket-lifecycle --bucket ${CI_BRANCH}.cwebber.net --lifecycle-configuration $LIFECYCLE_CONFIG
 
 fi
-
